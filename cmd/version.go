f610534223eb73566f9686f1313c060ba25b54bd
Kevin Townsend <kevin.townsend@analog.com>
2023-11-07 12:50:05 +0100

    cmd: version: Add commit hash and timestamp
    Adds the current commit hash to the version number, as well as
    and RFC822-format date and time string.
    
    Signed-off-by: Kevin Townsend <kevin.townsend@analog.com>

--- cmd/version.go
+++ cmd/version.go
@@ -2,20 +2,33 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
+// Requires Go 1.18+
+var Commit = func() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+	}
+	return ""
+}()
+
 var (
-	version    string = "v0.0.0"    // Semantic version number
-	buildDate  string = "undefined" // Build date and time
+	version    string = "0.1.0" // Semantic version number
 	versionCmd        = &cobra.Command{
 		Use:   "version",
 		Short: "Version and build details",
 		Long:  ``,
 		Run: func(_ *cobra.Command, _ []string) {
-			fmt.Printf("%s [%s]\n", version, buildDate)
-			return
+			now := time.Now()
+			fmt.Printf("%s-%s [%s]\n", version, Commit, now.Format(time.RFC822))
 		},
 	}
 )
